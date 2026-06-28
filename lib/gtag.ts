6cc6398099337205c851068dff3293ae3764bf10
mike1160 <leerthaismetmij@gmail.com>
2026-06-28 09:09:51 +0700

    fix: hardcode correct Google Ads conversion label (env var not resolving correctly during build)

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -3,8 +3,6 @@
  * {@link https://support.google.com/google-ads/answer/6331314}
  */
 
-console.log('BUILD-TIME CHECK:', process.env.NEXT_PUBLIC_GOOGLE_ADS_CONVERSION_LABEL);
-
 export const GOFUNDME_CAMPAIGN_URL =
   "https://www.gofundme.com/f/300-dogs-fighting-to-survive-in-thailand-be-their-hope";
 
@@ -51,7 +49,7 @@ export function gtagReportConversion(url?: string, _legacy?: unknown): boolean {
   if (url) setTimeout(callback, 1000);
 
   window.gtag("event", "conversion", {
-    send_to: `${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}/${process.env.NEXT_PUBLIC_GOOGLE_ADS_CONVERSION_LABEL}`,
+    send_to: `${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}/P0_YCKag8sYcEIWmuaND`,
     value: 1.0,
     currency: "USD",
     event_callback: callback,
